9238401d7386fa45ec561f1549a7352446947ecb
BanJo06 <vannejoedmaranan10@gmail.com>
2026-04-14 20:26:36 +0800

    v0.19.17

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,12 @@
 import { Stack } from "expo-router";
+import { View } from "react-native";
 import "../app/globals.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    // 'flex-1' makes it grow, 'min-h-screen' (or h-full) ensures it fills the browser
+    <View className="flex-1 min-h-screen">
+      <Stack screenOptions={{ headerShown: false }} />
+    </View>
+  );
 }
